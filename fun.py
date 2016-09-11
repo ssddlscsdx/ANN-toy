7aef8e7268061e5ec6ba5ff9b8f559246ade336b
Shen <pg@pgtekiMacBook-Pro.local>
2016-09-11 14:44:35 +0800

    add fun.py, which is shuang

---
+++ fun.py
@@ -0,0 +1,25 @@
+
+import numpy as np
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_deriv(x):
+    return sigmoid(x)*(1-sigmoid(x))
+
+input=2
+
+
+#strHello = "the length of (%s) is %d" %('Hello World',len('Hello World'))
+#print strHello
+
+
+for num in range(-5,5,1):
+    result = sigmoid(num);
+    result_de = sigmoid_deriv(num)
+    print 'The simgmoid of %d is %.2f, The derivative of sigmoid of %d is %.2f' %(num,result, num,result_de)
+
+
+print 'This is when all comes to an end'
+#output="%s wocao is it this %s, %.5f" %("ganniniang","hahahhahah",result_de)
+#print output
